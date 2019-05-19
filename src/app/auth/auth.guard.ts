9e5d586d7b23a3ad7639f1ebaa49bef751f97ebd
thiago-glauco <thiagoglauco@yahoo.com.br>
2019-05-18 22:49:01 -0300

    2 files modified

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -3,15 +3,18 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-
-  constructor(private authService: AuthService, private router: Router) {
-
+  user;
+  constructor(private authService: AuthService, private router: Router,  private afAuth: AngularFireAuth ) {
+    this.user = afAuth.user.subscribe( user => console.dir(this.user));
   }
   canActivate(): Observable<boolean> | boolean {
-    return this.authService.user.pipe(
+    /*return this.authService.user.pipe(
       map( user => {
         if (user.uid){
           console.log("user is true");
@@ -21,7 +24,7 @@ export class AuthGuard implements CanActivate {
           return false
         }
       })
-    );
-    //return false;
+    );*/
+ return true;
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,24 +4,33 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
-  user: Observable<firebase.User>
-  
+  user :Observable<firebase.User>
+  userId;
+  userEmail;
+
   constructor( private router: Router, private afAuth: AngularFireAuth ) {
     this.user = afAuth.authState;
+    this.user.pipe(
+      map( (user) => { this.userEmail = user.email})
+    )
+    console.log("user is", this.user);
   }
 
   login(){
-    this.afAuth.auth.signInWithPopup( new firebase.auth.GoogleAuthProvider())
-      .then( () => {
-        console.log("loguedin")
-        this.router.navigate([`/dashboard`])})
-      .catch( error => console.log('authError => ', error))
+    this.afAuth.auth.signInWithPopup( new auth.GoogleAuthProvider());
+    this.user.pipe(
+      map( (user) => { this.userEmail = user.email})
+    )
   }
 
   logout() {
+    this.user.pipe(
+      map( (user) => { console.log("good bye " + user.email)})
+    )
     this.afAuth.auth.signOut();
     this.router.navigate([`/home`]);
   }
